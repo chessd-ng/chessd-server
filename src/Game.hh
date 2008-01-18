83587624e43f73ebce38a92486e57c3a928f7bee
Raphael Ribas <rhr05@c3sl.ufpr.br>
2008-01-18 17:46:08 -0200

    Cleaned: Old code removed.

--- src/Game.hh
+++ src/Game.hh
@@ -8,63 +8,6 @@
 #include "Rating.hh"
 #include "XML/Xml.hh"
 
-/*typedef std::map<std::string, std::string> AttributeMap;
-
-typedef AttributeMap PlayerState;
-typedef std::vector<PlayerState> PlayerStateList;
-
-class BoardState {
-	public:
-		BoardState() { }
-
-		const AttributeMap& attributes() const { return this->_attributes; }
-		AttributeMap& attributes() { return this->_attributes; }
-
-		const PlayerStateList& players() const { return this->_players; }
-		PlayerStateList& players() { return this->_players; }
-
-		void swap(BoardState& b) { 
-			std::swap(this->attributes(), b.attributes());
-			std::swap(this->players(), b.players());
-		}
-	private:
-		AttributeMap _attributes;
-		PlayerStateList _players;
-};
-
-typedef std::vector<BoardState> BoardStateList;
-
-class GameState {
-	public:
-		GameState() { }
-
-		const AttributeMap& attributes() const { return this->_attributes; }
-		AttributeMap& attributes() { return this->_attributes; }
-
-		const BoardStateList& boards() const { return this->_boards; }
-		BoardStateList& boards() { return this->_boards; }
-
-		void swap(GameState& g) { 
-			std::swap(this->attributes(), g.attributes());
-			std::swap(this->boards(), g.boards());
-		}
-	private:
-		AttributeMap _attributes;
-		BoardStateList _boards;
-};
-*/
-
-/* Swap overloads */
-/*namespace std {
-	inline void swap(BoardState& b1, BoardState& b2) {
-		b1.swap(b2);
-	}
-	inline void swap(GameState& g1, GameState& g2) {
-		g1.swap(g2);
-	}
-}
-*/
-
 enum TeamResult {
 	WINNER,
 	DRAWER,
