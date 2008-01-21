c4b748f4ca0a22fea272691925fb6c7617e7982d
Raphael Ribas <rhr05@c3sl.ufpr.br>
2008-01-21 15:31:58 -0200

    Modified: A bit of code clean up.

--- src/XMPP/Roster.cc
+++ src/XMPP/Roster.cc
@@ -5,62 +5,62 @@
 using namespace std;
 
 namespace XMPP {
-	Roster::Roster(const StanzaHandler& stanza_sender, const ChangeStatusHandler& status_handler) :
-		stanza_sender(stanza_sender),
-		status_handler(status_handler) { }
+    Roster::Roster(const StanzaHandler& stanza_sender, const ChangeStatusHandler& status_handler) :
+        stanza_sender(stanza_sender),
+        status_handler(status_handler) { }
 
-	Roster::~Roster() { }
+    Roster::~Roster() { }
 
-	void Roster::handlePresence(Stanza* stanza) {
-		if(stanza->subtype().empty()) {
-			this->setUserStatus(stanza->from(), true);
-			delete stanza;
-		} else if(stanza->subtype() == "unavailable") {
-			this->setUserStatus(stanza->from(), false);
-			delete stanza;
-		} else {
-			stanza = Stanza::createErrorStanza(stanza, "modify", "bad-request");
-			this->stanza_sender(stanza);
-		}
-	}
+    void Roster::handlePresence(Stanza* stanza) {
+        if(stanza->subtype().empty()) {
+            this->setUserStatus(stanza->from(), true);
+            delete stanza;
+        } else if(stanza->subtype() == "unavailable") {
+            this->setUserStatus(stanza->from(), false);
+            delete stanza;
+        } else {
+            stanza = Stanza::createErrorStanza(stanza, "modify", "bad-request");
+            this->stanza_sender(stanza);
+        }
+    }
 
-	void Roster::setUserStatus(const Jid& user, bool available) {
-		this->status_handler(user, available);
-		std::map<Jid, UserInfo>::iterator it = this->users.find(user);
-		if(available)
-			std::cout << user.full() << " is Available" << endl;
-		else
-			std::cout << user.full() << " is Unavailable" << endl;
-		if(it == this->users.end()) {
-			this->users.insert(make_pair(user, UserInfo(available)));
-		} else {
-			it->second.available = available;
-			if(not it->second.handler.empty())
-				it->second.handler(available);
-		}
-	}
+    void Roster::setUserStatus(const Jid& user, bool available) {
+        this->status_handler(user, available);
+        user_map::iterator it = this->users.find(user);
+        if(available)
+            std::cout << user.full() << " is Available" << endl;
+        else
+            std::cout << user.full() << " is Unavailable" << endl;
+        if(it == this->users.end()) {
+            this->users.insert(make_pair(user, UserInfo(available)));
+        } else {
+            it->second.available = available;
+            if(not it->second.handler.empty())
+                it->second.handler(available);
+        }
+    }
 
-	void Roster::insertUserMonitor(const Jid& user, const UserStatusHandler& handler) {
-		std::map<Jid, UserInfo>::iterator it = this->users.find(user);
-		if(it == this->users.end()) {
-			this->users.insert(make_pair(user,UserInfo(handler)));
-			handler(false);
-		} else {
-			it->second.handler = handler;
-			it->second.handler(it->second.available);
-		}
-	}
+    void Roster::insertUserMonitor(const Jid& user, const UserStatusHandler& handler) {
+        user_map::iterator it = this->users.find(user);
+        if(it == this->users.end()) {
+            this->users.insert(make_pair(user,UserInfo(handler)));
+            handler(false);
+        } else {
+            it->second.handler = handler;
+            it->second.handler(it->second.available);
+        }
+    }
 
-	void Roster::removeUserMonitor(const Jid& user) {
-		this->users.find(user)->second.handler.clear();
-	}
+    void Roster::removeUserMonitor(const Jid& user) {
+        this->users.find(user)->second.handler.clear();
+    }
 
-	bool Roster::isUserAvailable(const Jid& user) const {
-		std::map<Jid, UserInfo>::const_iterator it = this->users.find(user);
-		if(it == this->users.end()) {
-			return false;
-		} else {
-			return it->second.available;
-		}
-	}
+    bool Roster::isUserAvailable(const Jid& user) const {
+        user_map::const_iterator it = this->users.find(user);
+        if(it == this->users.end()) {
+            return false;
+        } else {
+            return it->second.available;
+        }
+    }
 };
--- src/XMPP/Roster.hh
+++ src/XMPP/Roster.hh
@@ -14,66 +14,68 @@
 
 namespace XMPP {
 
-	typedef boost::function<void (bool available)> UserStatusHandler;
-	typedef boost::function<void (const XMPP::Jid& jid, bool available)> ChangeStatusHandler;
+    typedef boost::function<void (bool available)> UserStatusHandler;
+    typedef boost::function<void (const XMPP::Jid& jid, bool available)> ChangeStatusHandler;
 
-	class Roster {
-		public:
+    class Roster {
+        public:
 
-			/*! \brief Constructor 
-			 *
-			 * \param stanza_sender is the function used to send stanzas
-			 * \param handler is called on every status change
-			 * */
-			Roster(const StanzaHandler& stanza_sender,
-					const ChangeStatusHandler& handler = ChangeStatusHandler());
+            /*! \brief Constructor 
+             *
+             * \param stanza_sender is the function used to send stanzas
+             * \param handler is called on every status change
+             * */
+            Roster(const StanzaHandler& stanza_sender,
+                    const ChangeStatusHandler& handler = ChangeStatusHandler());
 
-			/*! \brief Destructor */
-			~Roster();
+            /*! \brief Destructor */
+            ~Roster();
 
-			/*! \brief Handle a presence stanza */
-			void handlePresence(Stanza* stanza);
+            /*! \brief Handle a presence stanza */
+            void handlePresence(Stanza* stanza);
 
-			/*! \brief Insert a user status monitor
-			 *
-			 * Insert a handler to be called on user status change. Current
-			 * user status is immediately notified.
-			 * \param user is the user's jid.
-			 * \param handler is the handler function.
-			 */
-			void insertUserMonitor(const Jid& user, const UserStatusHandler& handler);
+            /*! \brief Insert a user status monitor
+             *
+             * Insert a handler to be called on user status change. Current
+             * user status is immediately notified.
+             * \param user is the user's jid.
+             * \param handler is the handler function.
+             */
+            void insertUserMonitor(const Jid& user, const UserStatusHandler& handler);
 
-			/*! \brief Remove a user monitor.
-			 *
-			 * \param user is the user's jid.
-			 */
-			void removeUserMonitor(const Jid& user);
+            /*! \brief Remove a user monitor.
+             *
+             * \param user is the user's jid.
+             */
+            void removeUserMonitor(const Jid& user);
 
-			/*! \brief Get the user availability
-			 *
-			 * \param user is the user's jid.
-			 * \return Returns the user availability.
-			 */
-			bool isUserAvailable(const Jid& user) const;
-		private:
-			struct UserInfo {
-				bool available;
-				UserStatusHandler handler;
-				UserInfo() : available(false) { }
-				UserInfo(bool available) : available(available) { }
-				UserInfo(UserStatusHandler handler) :
-					available(false),
-					handler(handler) { }
-			};
+            /*! \brief Get the user availability
+             *
+             * \param user is the user's jid.
+             * \return Returns the user availability.
+             */
+            bool isUserAvailable(const Jid& user) const;
+        private:
+            struct UserInfo {
+                bool available;
+                UserStatusHandler handler;
+                UserInfo() : available(false) { }
+                UserInfo(bool available) : available(available) { }
+                UserInfo(UserStatusHandler handler) :
+                    available(false),
+                    handler(handler) { }
+            };
 
-			void setUserStatus(const Jid& user, bool available);
+            void setUserStatus(const Jid& user, bool available);
 
-			std::map<Jid, UserInfo> users;
+            typedef std::map<Jid, UserInfo> user_map;
 
-			StanzaHandler stanza_sender;
+            user_map users;
 
-			ChangeStatusHandler status_handler;
-	};
+            StanzaHandler stanza_sender;
+
+            ChangeStatusHandler status_handler;
+    };
 }
 
 #endif
