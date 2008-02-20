38d60ac37911dd00bf06f452f471fd401b19bffe
Eduardo Ribas <ear07@c3sl.ufpr.br>
2008-02-20 16:49:23 -0300

    fix: the endreason is now more precise

--- src/GameChess.cc
+++ src/GameChess.cc
@@ -124,6 +124,12 @@ void GameChess::adjourn() {
 }
 
 bool GameChess::done(const Util::Time& current_time) {
+	if(chess.verifyCheckMate() or this->_resign!=Chess::UNDEFINED)
+		return true;
+	else if(this->_draw==true)
+		return true;
+	else if(chess.verifyDraw()==true)
+		return true;
 	if(this->chess.numberOfTurns() >= 2)
 		foreach(it,standard_player_map)
 			if(it->second->time+(this->colormap[it->first]==this->chess.turn()?this->time_of_last_move-current_time:Util::Time()) <= Util::Time()) {
@@ -132,12 +138,6 @@ bool GameChess::done(const Util::Time& current_time) {
 					this->_history=history_saved.getTag();
 				return true;
 			}
-	if(chess.verifyCheckMate() or this->_resign!=Chess::UNDEFINED)
-		return true;
-	else if(this->_draw==true)
-		return true;
-	else if(chess.verifyDraw()==true)
-		return true;
 	return false;
 }
 
