765e05e26de22c46eca23caf184fe09e73e4ca57
Raphael Ribas <rhr05@c3sl.ufpr.br>
2008-04-10 23:13:37 -0300

    Fix: WriteLock destructor was missing.

--- src/Threads/SafeObject.hh
+++ src/Threads/SafeObject.hh
@@ -23,43 +23,48 @@
 
 namespace Threads {
 
-	template <class T>
-		class SafeObject {
-			private:
-				T object;
-				RwLock rwlock;
-			public:
+    template <class T> class SafeObject {
+        public:
 
-				SafeObject() { }
-				SafeObject(const T& object) : object(object) { }
-				~SafeObject() { }
+            SafeObject() { }
+            SafeObject(const T& object) : object(object) { }
+            ~SafeObject() { }
 
-				T& getWriteLock() {
-					this->rwlock.wrlock();
-					return object;
-				}
-				const T& getReadLock() {
-					this->rwlock.rdlock();
-					return object;
-				}
-				void releaseLock() {
-					this->rwlock.unlock();
-				}
-		};
+            T& getWriteLock() {
+                this->rwlock.wrlock();
+                return object;
+            }
+            const T& getReadLock() {
+                this->rwlock.rdlock();
+                return object;
+            }
+            void releaseLock() {
+                this->rwlock.unlock();
+            }
+
+        private:
+            T object;
+            RwLock rwlock;
+    };
 
 	template <class T> class ReadLock {
 		public:
 			ReadLock(SafeObject<T>& safe_object) :
 				safe_object(safe_object),
-				object(safe_object.getReadLock())
-		{ }
+				object(safe_object.getReadLock()) { }
 
 			~ReadLock() {
 				safe_object.releaseLock();
 			}
+
 			const T* operator->() const {
 				return &this->object;
 			}
+
+            const T& operator*() const {
+                return this->object;
+            }
+
 		private:
 			SafeObject<T>& safe_object;
 			const T& object;
@@ -69,18 +74,28 @@ namespace Threads {
 		public:
 			WriteLock(SafeObject<T>& safe_object) :
 				safe_object(safe_object),
-				object(safe_object.getWriteLock())
-		{ }
+				object(safe_object.getWriteLock()) { }
 
-			WriteLock() {
+			~WriteLock() {
 				safe_object.releaseLock();
 			}
+
 			T* operator->() {
 				return &this->object;
 			}
+
 			const T* operator->() const {
 				return &this->object;
 			}
+
+            T& operator*() {
+                return this->object;
+            }
+
+            const T& operator*() const {
+                return this->object;
+            }
+
 		private:
 			SafeObject<T>& safe_object;
 			T& object;
